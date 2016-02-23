085b1adb3f64da45cf8987f751e37e0db4b00278
littlelove2013 <gongcheng_nankai@163.com>
2016-02-23 14:18:11 +0800

    insert text

---
+++ Conf/config_database.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 龚成
+ * Date: 2016/2/23
+ * Time: 20:59
+ *
+ * 引用于Disucz开源论坛的config/config_global.php
+ *
+ */
+
+define("DB_CONFIG",1);
+$_config = array();
+
+// ----------------------------  CONFIG DB  ----------------------------- //
+$_config['db']['1']['dbhost'] = 'localhost';
+$_config['db']['1']['dbuser'] = 'root';
+$_config['db']['1']['dbpw'] = '159753159753';
+$_config['db']['1']['dbcharset'] = 'utf8';
+$_config['db']['1']['dbname'] = 'gcDiscuzForum';
+$_config['db']['1']['tablepre'] = 'gcDiscuzForum_';
+
+?>
